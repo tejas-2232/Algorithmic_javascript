2a4bba4305edcdf0e8cb13c657753611f69aa471
Tejas <tejasbachhav98@gmail.com>
2020-09-23 00:00:03 +0530

    vowel counter using iterative approach updated

--- Vowel_counter/iterative_appr.js
+++ Vowel_counter/iterative_appr.js
@@ -1,5 +1,15 @@
 const vowel = ["a","e","i","o","u"];
 
 function vowelcnt(text){
-    
+ let counter= 0;
+ 
+ //Loop through text to test if each character is a vowel
+ for (let letter of text.toLowerCase()){
+     if(vowel.includes(letter)){
+         counter++;
+     }
+ }
+ // Return number of vowels
+
+ return counter;
 }
