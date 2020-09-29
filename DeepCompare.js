731975da07719149cecd6282cda5910fd38a3a78
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2020-09-29 19:51:55 +0100

    Added deepCompare snippet

---
+++ DeepCompare.js
@@ -0,0 +1,25 @@
+//deep compare function for multidimensional array
+const deepCompare = (a, b) => {
+  let res = true
+    
+  if(Array.isArray(a) && Array.isArray(b))
+  {
+    let count = 0;
+
+    while(res && count < a.length)
+    {
+      res = !a.some( (val, i) => {
+        return !deepCompare(val, b[i])
+      })
+
+      count++
+    }
+  }
+  else
+  {
+      res = (a === b)
+  }
+
+  return res
+
+}
