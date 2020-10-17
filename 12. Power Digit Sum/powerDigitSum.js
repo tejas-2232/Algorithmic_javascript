a0602554d8d4921720f2192c539c283400e47cf9
ranggaadi <adirangga922@gmail.com>
2020-10-17 20:06:16 +0700

    commit for rebase

--- 12. Power Digit Sum/powerDigitSum.js
+++ 12. Power Digit Sum/powerDigitSum.js
@@ -10,7 +10,9 @@ exponential result.
 
 function powerDigitSum(num, pow){
     let sum = 0
-    for (let digit of Math.pow(num, pow).toString()){
+
+    // Step 1: Iterate over
+    for (let digit of Math.pow(num, pow).toString()){ 
       if (digit === '.'){  //checking for point (if exponential is negative)
         continue;
       }
