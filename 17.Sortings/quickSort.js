d7f8a7b878974012758ca3d089e684ad2ae473ad
Ruchi <i.ruchiagrawal0612.com>
2025-10-18 14:51:13 +0530

    Add sorting and dynamic programming algorithms in JavaScript

---
+++ 17.Sortings/quickSort.js
@@ -0,0 +1,21 @@
+function quickSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const arr = [7, 8, 1, 2, 6, 3, 5];
+const sortedArr = quickSort(arr);
+console.log(sortedArr);
